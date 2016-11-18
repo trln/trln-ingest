8dd16e73d72176ad5e0f048d6b650389c9ea31cc
vagrant <vagrant@localhost.localdomain>
2016-11-18 18:02:29 +0000

    Fix routing for JSON ingest

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root to: "transactions#index"
 
-  post '/ingest/:owner', to: 'transactions#ingest_json', constraints: { content_type: :json }
+  post '/ingest/:owner', to: 'transactions#ingest_json', constraints: { content_type: 'application/json' }
   post '/ingest/:owner', to: 'transactions#ingest_zip', constraints: { content_type: 'application/zip' }
 
   get   '/ingest/:owner', to: 'transactions#ingest_form' , as: 'ingest_form'
