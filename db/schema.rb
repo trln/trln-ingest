ae5afc1ed7b639a0a1b31da31a1e091dedf68fa9
Adam Constabaris <adam_constabaris@ncsu.edu>
2018-06-13 16:05:31 -0400

    update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120160501) do
+ActiveRecord::Schema.define(version: 20180613192256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20171120160501) do
     t.string "updated_by"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "deleted", default: false
     t.index ["id"], name: "index_documents_on_id", unique: true
   end
 
@@ -55,6 +56,7 @@ ActiveRecord::Schema.define(version: 20171120160501) do
     t.boolean "admin", default: false, null: false
     t.boolean "approved", default: false, null: false
     t.string "authentication_token", limit: 30
+    t.string "primary_institution"
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
