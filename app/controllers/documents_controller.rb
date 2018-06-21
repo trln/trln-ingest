63d32266adad9d0f91bee7727d49ed5959e28ec5
Adam Constabaris <adam_constabaris@ncsu.edu>
2018-06-21 10:07:48 -0400

    Add logic to show when a document is deleted

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -12,10 +12,10 @@ class DocumentsController < ApplicationController
   end
 
   def search
-    logger.info "Params: #{params[:id]}"
+    logger.debug "Params: #{params[:id]}"
     begin
       @doc = Document.find(params[:id])
-      logger.info "here's the doc: #{@doc}"
+      logger.debug "here's the doc: #{@doc}"
       show
     rescue ActiveRecord::RecordNotFound
       not_found
@@ -27,9 +27,34 @@ class DocumentsController < ApplicationController
     respond_to do |format|
       format.html do
         @content = @doc.content.to_ostruct_deep
-        @solr = fetch_solr(@doc.id)
-        @enriched = prepare_for_ingest(@doc.content)
-        @luke = fetch_luke_doc(@doc.id)
+        @solr = begin
+                  fetch_solr(@doc.id)
+                rescue StandardError
+                  {}
+                end
+        @enriched = begin
+                      prepare_for_ingest(@doc.content)
+                    rescue StandardError
+                      {}
+                    end
+        @luke = begin
+                  fetch_luke_doc(@doc.id)
+                rescue StandardError
+                  {}
+                end
+        if @doc.deleted?
+          flash[:notice] = 'This record has been deleted.'
+          unless @solr.empty?
+            delay = ((Time.current - @doc.updated_at) / 1.hour).round
+            if delay > 1
+              flash[:alert] = 'This record still appears in Solr' unless @solr.empty?
+            else
+              flash[:notice] << "\nIt should be removed from the index soon."
+            end
+          else
+
+          end
+        end
         render 'show'
       end
       format.json do
