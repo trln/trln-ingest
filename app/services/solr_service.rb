833a3e7a0270a9193b9b90c0ac1f76642aafe7f2
Adam Constabaris <adam_constabaris@ncsu.edu>
2018-06-21 10:06:02 -0400

    expand solr_service to allow use of more than one backend

--- app/services/solr_service.rb
+++ app/services/solr_service.rb
@@ -2,46 +2,61 @@ require 'spofford'
 require 'rsolr'
 
 class SolrService
-    class << self; attr_accessor :config end
+  class << self; attr_accessor :config end
 
-    @config = YAML.load_file(Rails.root.join('config', 'solr.yml'))[Rails.env].to_ostruct_deep
+  @config = YAML.load_file(Rails.root.join('config', 'solr.yml'))[Rails.env].to_ostruct_deep
 
-    attr_accessor :url, :collection, :client
-    
-    # Create a new connection to a Solr collection.
-    # if passed a block, this object will be the  block's parameter.
-    def initialize(collection='shared')
-        @collection= SolrService.config.collections[collection]
-        raise "No collection with the logical name '#{collection}' defined in environment '#{Rails.env}'" unless @collection
-        @url = URI.join(SolrService.config.url, @collection).to_s
-        @client = RSolr.connect :url => url
-        if block_given?
-            yield self
-        end
+  attr_accessor :url, :collection, :client
+
+  # Create a new connection to a Solr collection.
+  # if passed a block, this object will be the  block's parameter.
+  def initialize(collection = 'trlnbib')
+    @collection = collection
+    @url = URI.join(self.class.config.url.sample, @collection).to_s
+    @client = RSolr.connect :url => url
+    yield self if block_given?
+  end
+
+  def count
+    response = @client.select params: { q: '*:*', rows: 0 }
+  end
+
+  def ping
+    clients = Hash[self.class.config.url.map do |host|
+      uri = URI.join(host, @collection)
+      hostname = uri.host
+      [hostname, client]
+    end]
+    clients.each_with_object({}) do |(host, client), out|
+      out[host] = client.get('admin/ping')
     end
+  end
 
-    # Deletes documents from the index by ID.
-    # always commits after running
-    # @param ids [Array<String>] an array of IDs to delete from the Solr index
-    def delete_by_ids(ids)
-        @client.delete_by_id(ids)
-        @client.commit()
-    end
+  # Deletes documents from the index by ID.
+  # always commits after running
+  # @param ids [Array<String>] an array of IDs to delete from the Solr index
+  def delete_by_ids(ids)
+    @client.delete_by_id(ids)
+    @client.commit
+  end
 
-    # Send an array of (concatenated) JSON files containing indexable documents
-    # to the update URL for the collection.
-    # @param files [Array<String>] filenames
-    # @param commit_interval [Integer] number of files to submit before issuing a commit; set to 0 to commit after all files are processed,
-    #    and to -1 to disable commiting entirely (manual commit).
-    def json_doc_update(files,commit_interval=0) 
-        count = 0
-        files.each do |filename|
-            @client.update(:path => 'update/json/docs', :headers => { 'Content-Type' => 'application/json' }, :data => File.read(filename))
-            count += 1
-            if commit_interval > 0 and (count % commit_interval) ==0 and 
-                @client.commit
-            end
-        end
-        @client.commit unless commit_interval == -1
+  # Send an array of (concatenated) JSON files containing indexable documents
+  # to the update URL for the collection.
+  # @param files [Array<String>] filenames
+  # @param commit_interval [Integer] number of files to submit before issuing
+  # a commit; set to 0 to commit after all files are processed,
+  #    and to -1 to disable commiting entirely (manual commit).
+  def json_doc_update(files, commit_interval = 0)
+    count = 0
+    files.each do |filename|
+      @client.update(
+        path: 'update/json/docs',
+        headers: { 'Content-Type' => 'application/json' },
+        data: File.read(filename)
+      )
+      count += 1
+      @client.commit if commit_interval > 0 && (qcount % commit_interval) == 0
     end
+    @client.commit unless commit_interval == -1
+  end
 end
