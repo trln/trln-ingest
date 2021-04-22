ce4991bfa4fce9ec3eb4a2e7defa15d7a5359ca0
Adam Constabaris <adam_constabaris@ncsu.edu>
2021-04-22 10:46:33 -0400

    add reindex/reingest feature to dashboard

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -8,4 +8,31 @@ module DashboardHelper
              end
     "list-group-item-#{suffix}"
   end
+
+  def institutions 
+    %w[duke nccu ncsu unc]
+  end
+
+  def range_query
+	  Transaction.where('updated_at >? AND updated_at <?', @reindex.from, @reindex.to)
+  end
+
+  def reindex_range
+	  q = range_query
+	  q = q.where(owner: @reindex.institution) unless @reindex.institution.nil? or @reindex.institution.blank?
+	  return q.count() unless @reindex.commit
+
+	  q.each do |tx|
+		  IndexingWorker.perform_async(tx.id)
+    end
+  end
+
+  def reingest_range(commit=false)
+	  q = range_query
+	  q = q.where(owner: @institution) unless @reindex.institution.nil?
+	  return q.count() unless @reindex.commit
+	  q.each do |tx|
+		  TransactionWorkder.perform_async(tx.id)
+	  end
+  end	
 end
