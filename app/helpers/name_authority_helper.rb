6f403e820b8d033e2a406ee8fd651e24158bdad1
Adam Constabaris <adam_constabaris@ncsu.edu>
2021-04-22 15:03:47 -0400

    fix tabs and some rubocop stuff

--- app/helpers/name_authority_helper.rb
+++ app/helpers/name_authority_helper.rb
@@ -1,29 +1,30 @@
+# frozen_string_literal: true
+
 require 'redis'
 
+# Helpers to assist with name authority ID lookup
 module NameAuthorityHelper
+  def do_lookup(val = false)
+    lookup_id = params.fetch(:lookup, val)
+    return [lookup_id, []] unless lookup_id
 
-  def do_lookup(v=false)
-	lookup_id = params.fetch(:lookup, v)
-	return [lookup_id, []] unless lookup_id
-	
-	lookup_id = canonicalize(lookup_id)
-	result = redis.get(lookup_id)
-	if result.nil?
-		return [lookup_id, []]
-	end
-	[ lookup_id, JSON.parse(result)]
+    lookup_id = canonicalize(lookup_id)
+    result = redis.get(lookup_id)
+    return [lookup_id, []] if result.nil?
+
+    [lookup_id, JSON.parse(result)]
   end
 
-  def canonicalize(v)
-	v = v.strip
-	return v.gsub(/[^a-zA-Z0-9:]/, '') if v.start_with?('lcnaf:')
-	
-	return "lcnaf:#{$1}" if v =~ /^https?:\/\/id\.loc\.gov\/authorities\/names\/([a-zA-Z0-9]+)/
+  def canonicalize(val)
+    val = val.strip
+    return val.gsub(/[^a-zA-Z0-9:]/, '') if val.start_with?('lcnaf:')
 
-	return "lcnaf:#{v.gsub(/\W/, '')}"
+    return "lcnaf:#{Regexp.last_match(1)}" if val =~ %r{^https?://id\.loc\.gov/authorities/names/([a-zA-Z0-9]+)}
+
+    "lcnaf:#{val.gsub(/\W/, '')}"
   end
-	
+
   def redis
-	@redis ||= Redis.new(url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379/0'))
+    @redis ||= Redis.new(url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379/0'))
   end
 end
