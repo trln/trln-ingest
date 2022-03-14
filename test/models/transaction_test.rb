5c15268a3ce0ebfa9d6d32f1a7c69f2ed85bdac0
Adam Constabaris <adam_constabaris@ncsu.edu>
2022-03-14 14:44:11 -0400

    fix failing test

--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class TransactionTest < ActiveSupport::TestCase
 
 	setup do 
+    @user = User.create(email:'ncsu@example.com', approved: true)
 		record = { id: "TRLN123456789", title_main: "nothing" }
 		@record_file = Tempfile.new(["add-spofford-test", ".json"]).path
 		File.open(@record_file, "w") do |f|
@@ -11,7 +12,7 @@ class TransactionTest < ActiveSupport::TestCase
 	end
 
   test 'stash_directory does not mutate over time' do
-  	t = Transaction.create(owner: 'ncsu', files: [@record_file])
+  	t = Transaction.create(owner: 'ncsu', user: @user, files: [@record_file])
   	t.stash!
   	t.save!
   	orig_stashdir = t.stash_directory
